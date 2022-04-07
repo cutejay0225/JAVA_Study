970e1445bec60edfa88dbc1e14f8b9f83684a16a
서예빈 <85054837+cutejay0225@users.noreply.github.com>
2022-04-07 15:48:44 +0900

    Rename 서예빈 20210845 (12).java to java2_8.java

---
+++ java2_8.java
@@ -0,0 +1,17 @@
+package java2_8;
+
+public class java2_8 {
+	public static void main(String[] srgs) { // 메인 메서드
+		int x = 300;
+		int y = 300;
+		// 변수 선언
+		
+		System.out.println("x = 300");
+		System.out.println("y = 300");
+		// x, y에 할당된 변수 출력
+		System.out.println("x를 42로 나눈 몫은" + (x/42) +", 나머지는" + (x%42) + "입니다.");
+		System.out.println("y를 38로 나눈 몫은" + (y/38) +", 나머지는" + (y%38) + "입니다.");
+		// x, y를 연산하여 결과를 모니터에 출력
+	}
+
+}
