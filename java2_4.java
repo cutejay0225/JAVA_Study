22f538d8c5749a52f9c0fb006b5a1c77bc690be6
서예빈 <85054837+cutejay0225@users.noreply.github.com>
2022-04-07 15:50:31 +0900

    Rename 서예빈 20210845 (8).java to java2_4.java

---
+++ java2_4.java
@@ -0,0 +1,17 @@
+package java2_4;
+// 새로 정의할 클래스 이름 기술
+public class java2_4 {
+	// 메인 메서드 기술
+	public static void main(String[] srgs) {
+		// 번수 선언하여 값 할당
+		int a = 8;
+		int b = 5;
+		
+		System.out.println("a + b = " + (a + b)); // 결과를 모니터에 출력 (a+b)
+		System.out.println("a - b = " + (a - b)); // 결과를 모니터에 출력 (a+b)
+		System.out.println("a * b = " + (a * b)); // 결과를 모니터에 출력 (a+b)
+		System.out.println("a / b = " + (a / b)); // 결과를 모니터에 출력 (a+b)
+		System.out.println("a % b = " + (a % b)); // 결과를 모니터에 출력 (a+b)
+	}
+
+}
