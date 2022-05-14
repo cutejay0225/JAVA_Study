b5547e52fa6bbdadb90a8c5f5f285bf25deea8b0
서예빈 <85054837+cutejay0225@users.noreply.github.com>
2022-05-14 23:30:51 +0900

    Add files via upload

---
+++ 연습.java
@@ -0,0 +1,12 @@
+
+public class 연습 {
+	public static void main(String str[]) {
+	// 변수 선언
+	int inta= 22250; //정수형 변수 선언
+	int intb= 32500;
+	float floatc;
+	floatc = inta + intb; //①
+	// 변수에 입력된 값 출력
+	System.out.println("정수형 합을 실수형 변수에 저장 : " + floatc);
+	}
+}
